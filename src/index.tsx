4b140969418e9423d444abde00ff88844bdc03a6
Mouaz El Molkey <moaz.mulki@outlook.com>
2022-10-12 14:32:56 +0300

    import bootstrap italia to the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'src/App.css'
-
+import "bootstrap-italia"
+import "bootstrap-italia/dist/css/bootstrap-italia.min.css"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
